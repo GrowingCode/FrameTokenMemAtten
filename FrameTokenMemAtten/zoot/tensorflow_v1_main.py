e41f38c34228025e606575befec2c23b2c2ce77a
yangy <yangy@192.168.150.1>
2020-03-16 00:38:15 +0800

    small change.

--- FrameTokenMemAtten/zoot/tensorflow_v1_main.py
+++ FrameTokenMemAtten/zoot/tensorflow_v1_main.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 if __name__ == '__main__':
 #   tf.debugging.set_log_device_placement(True)
   check_or_store_configs()
-  with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(allow_soft_placement=False)) as sess:
+  with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(allow_soft_placement=True)) as sess:
     runner = ModelRunner(sess)
     init_op = tf.compat.v1.global_variables_initializer()
     sess.run(init_op)
