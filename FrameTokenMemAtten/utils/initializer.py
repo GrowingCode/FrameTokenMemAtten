66a428a087a17f0692a696eef3be872fc66b871c
yangy <yangy@DESKTOP-E1U9CQQ>
2020-05-20 12:43:22 +0800

    small change.

--- FrameTokenMemAtten/utils/initializer.py
+++ FrameTokenMemAtten/utils/initializer.py
@@ -1,11 +1,12 @@
-import tensorflow as tf
-from metas.non_hyper_constants import float_type
 from metas.hyper_settings import initialize_range, initialize_seed_base
-
+from metas.non_hyper_constants import float_type
+import tensorflow as tf
 
 variable_initializer_seed = 13
 
 def random_uniform_variable_initializer(mt, bs, shape, ini_range=initialize_range):
+  if ini_range == 0.0:
+    return zero_variable_initializer(shape)
   return tf.random.uniform(shape, minval=-ini_range, maxval=ini_range, dtype=float_type, seed=mt*variable_initializer_seed+bs+initialize_seed_base)
 
 def one_variable_initializer(shape):
